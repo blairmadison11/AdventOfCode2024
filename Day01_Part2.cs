3d95fc6d472dbaa3362d288399dc8cea31ae0a0a
Blair Durkee <blairmadison11@gmail.com>
2024-12-01 18:21:40 -0500

    Update Day01_Part2.cs

--- Day01_Part2.cs
+++ Day01_Part2.cs
@@ -1,11 +1,11 @@
 using System.Text.RegularExpressions;
 
 var lines = File.ReadAllLines("input.txt");
-var list1 = lines.Select(l => int.Parse(Regex.Match(l, @"^\d+").Value)).Order().ToArray();
-var list2 = lines.Select(l => int.Parse(Regex.Match(l, @"\d+$").Value)).Order().ToArray();
-var sum = 0;
-for (int i = 0; i < list1.Length; ++i)
+var list1 = lines.Select(l => int.Parse(Regex.Match(l, @"^\d+").Value)).ToArray();
+var dict2 = new Dictionary<int, int>();
+foreach (string line in lines)
 {
-    sum += list1[i] * list2.Aggregate(0, (a, c) => list1[i] == c ? a + 1 : a);
+    var num = int.Parse(Regex.Match(line, @"\d+$").Value);
+    dict2[num] = dict2.GetValueOrDefault(num) + 1;
 }
-Console.WriteLine(sum);
+Console.WriteLine(list1.Aggregate(0, (a, c) => a + (c * dict2.GetValueOrDefault(c))));
