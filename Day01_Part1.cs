c9b5629b22b610381212be53643022468850ad51
Blair Durkee <blairmadison11@gmail.com>
2024-12-01 18:29:06 -0500

    Update Day01_Part1.cs

--- Day01_Part1.cs
+++ Day01_Part1.cs
@@ -3,9 +3,4 @@ using System.Text.RegularExpressions;
 var lines = File.ReadAllLines("input.txt");
 var list1 = lines.Select(l => int.Parse(Regex.Match(l, @"^\d+").Value)).Order().ToArray();
 var list2 = lines.Select(l => int.Parse(Regex.Match(l, @"\d+$").Value)).Order().ToArray();
-var sum = 0;
-for (int i = 0; i < list1.Length; ++i)
-{
-    sum += Math.Abs(list1[i] - list2[i]);
-}
-Console.WriteLine(sum);
+Console.WriteLine(list1.Zip(list2).Aggregate(0, (a, c) => a + Math.Abs(c.First - c.Second)));
