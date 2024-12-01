5c222ca7577e3bd337ea4658f0c701bb93888c44
Blair Durkee <blairmadison11@gmail.com>
2024-12-01 00:29:53 -0500

    Create Day01_Part1.cs

---
+++ Day01_Part1.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+var list1 = lines.Select(l => int.Parse(Regex.Match(l, @"^\d+").Value)).Order().ToArray();
+var list2 = lines.Select(l => int.Parse(Regex.Match(l, @"\d+$").Value)).Order().ToArray();
+var sum = 0;
+for (int i = 0; i < list1.Length; ++i)
+{
+    Console.WriteLine("{0}   {1}", list1[i], list2[i]);
+    sum += Math.Abs(list1[i] - list2[i]);
+}
+Console.WriteLine(sum);
