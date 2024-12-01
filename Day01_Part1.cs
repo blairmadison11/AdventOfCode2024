5a0763c02ff315bbf0233af79b37f36595860d02
Blair Durkee <blairmadison11@gmail.com>
2024-12-01 18:32:32 -0500

    Update Day01_Part1.cs

--- Day01_Part1.cs
+++ Day01_Part1.cs
@@ -1,6 +1,5 @@
 using System.Text.RegularExpressions;
 
 var lines = File.ReadAllLines("input.txt");
-var list1 = lines.Select(l => int.Parse(Regex.Match(l, @"^\d+").Value)).Order().ToArray();
-var list2 = lines.Select(l => int.Parse(Regex.Match(l, @"\d+$").Value)).Order().ToArray();
-Console.WriteLine(list1.Zip(list2).Aggregate(0, (a, c) => a + Math.Abs(c.First - c.Second)));
+var gl = (string rgx) => lines.Select(l => int.Parse(Regex.Match(l, rgx).Value)).Order();
+Console.WriteLine(gl(@"^\d+").Zip(gl(@"\d+$")).Aggregate(0, (a, c) => a + Math.Abs(c.First - c.Second)));
