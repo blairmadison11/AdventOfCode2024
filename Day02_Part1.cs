7a893c39455b163f549bef65e6c6900307857f59
Blair Durkee <blairmadison11@gmail.com>
2024-12-02 23:40:23 -0500

    Update Day02_Part1.cs

--- Day02_Part1.cs
+++ Day02_Part1.cs
@@ -1,3 +1,4 @@
+var lists = File.ReadAllLines("input.txt").Select(l => l.Split(' ').Select(int.Parse).ToArray()).ToArray();
 var isOrdered = (int[] nums) =>
 {
     var pairs = nums.Zip(nums.Skip(1));
@@ -6,5 +7,5 @@ var isOrdered = (int[] nums) =>
 };
 var isSmallDiff = (int[] nums) => nums.Zip(nums.Skip(1)).All(x => Math.Abs(x.First - x.Second) is >= 1 and <= 3);
 var isSafe = (int[] nums) => isOrdered(nums) && isSmallDiff(nums);
-var lists = File.ReadAllLines("input.txt").Select(l => l.Split(' ').Select(int.Parse).ToArray()).ToArray();
+
 Console.WriteLine(lists.Aggregate(0, (a, c) => a + (isSafe(c) ? 1 : 0)));
