e2010dafb5c6f4ec39c807692c04e6fdaf9286ee
Blair Durkee <blairmadison11@gmail.com>
2024-12-03 00:12:07 -0500

    Update Day03_Part1.cs

--- Day03_Part1.cs
+++ Day03_Part1.cs
@@ -1,3 +1,3 @@
 using System.Text.RegularExpressions;
 
-Console.WriteLine(Regex.Matches(File.ReadAllText("input.txt"), @"mul\((\d\d?\d?),(\d\d?\d?)\)").Select(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value)).Aggregate(0, (a, c) => a + c));
+Console.WriteLine(Regex.Matches(File.ReadAllText("input.txt"), @"mul\((\d\d?\d?),(\d\d?\d?)\)").Select(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value)).Sum());
