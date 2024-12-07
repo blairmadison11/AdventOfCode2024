c1d969d714bd01458bc1bdace55c6564af8e14e4
Blair Durkee <blairmadison11@gmail.com>
2024-12-06 19:52:12 -0500

    Update Day06_Part1.cs

--- Day06_Part1.cs
+++ Day06_Part1.cs
@@ -1,37 +1,97 @@
-var dirs = new Dictionary<char, int>() { { '^', 0 }, { '>', 1 }, { 'v', 2 }, { '<', 3 } };
-var dirVecs = new (int, int)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
-var map = File.ReadAllLines("input.txt").Select(x => x.ToCharArray()).ToArray();
-var y = Array.FindIndex(map, r => Array.FindIndex(r, c => dirs.ContainsKey(c)) != -1);
-var x = Array.FindIndex(map[y], c => dirs.ContainsKey(c));
-var pos = (x, y);
-var next = pos;
-var dir = dirs[map[y][x]];
-var done = false;
-var count = 0;
-while (!done)
+var lines = File.ReadAllLines("input.txt");
+var nodeGrid = new Node[lines.Length, lines[0].Length];
+Node? cur  = null;
+for (int i = 0; i < lines.Length; ++i)
 {
-    next = (pos.x + dirVecs[dir].Item1, pos.y + dirVecs[dir].Item2);
-    if (next.y < 0 || next.y >= map.Length || next.x < 0 || next.x >= map[next.y].Length)
+    for (int j = 0; j < lines[i].Length; ++j)
     {
-        if (map[pos.y][pos.x] != 'X')
+        Node node = new Node(j, i);
+        nodeGrid[i, j] = node;
+        if (i > 0) node.AddAdjacent(Direction.North, nodeGrid[i - 1, j]);
+        if (j > 0) node.AddAdjacent(Direction.West, nodeGrid[i, j - 1]);
+        if (lines[i][j] == '.')
         {
-            map[pos.y][pos.x] = 'X';
-            ++count;
+            node.Type = NodeType.Path;
         }
-        done = true;
-    }
-    else if (map[next.y][next.x] == '#')
-    {
-        dir = (dir + 1) % 4;
-    }
-    else
-    {
-        if (map[pos.y][pos.x] != 'X')
+        else if (lines[i][j] == '#')
         {
-            map[pos.y][pos.x] = 'X';
-            ++count;
+            node.Type |= NodeType.Obstacle;
         }
-        pos = next;
+        else
+        {
+            node.Type = NodeType.Path;
+            node.SetDirection(lines[i][j]);
+            cur = node;
+        }
     }
 }
-Console.WriteLine(count);
+
+var visitedNodes = new HashSet<Node>();
+while (cur != null)
+{
+    visitedNodes.Add(cur);
+    cur = cur.GetNext();
+}
+Console.WriteLine(visitedNodes.Count);
+
+// *** Classes ***
+enum Direction { None, North, South, East, West };
+enum NodeType { Path, Obstacle };
+class Node
+{
+    static private Dictionary<Direction, Direction> OppDir = new Dictionary<Direction, Direction>() { { Direction.North, Direction.South },
+        { Direction.South, Direction.North }, { Direction.East, Direction.West }, { Direction.West, Direction.East } };
+    static private Dictionary<Direction, Direction> RightTurnDir = new Dictionary<Direction, Direction>() { { Direction.North, Direction.East },
+        { Direction.East, Direction.South }, { Direction.South, Direction.West }, { Direction.West, Direction.North } };
+    static private Dictionary<char, Direction> CharDir = new Dictionary<char, Direction> { { '^', Direction.North },
+        { '>', Direction.East }, { 'v', Direction.South}, { '<', Direction.West } };
+
+    public NodeType Type;
+    private static Direction dir = Direction.None;
+
+    private int x, y;
+    private Dictionary<Direction, Node> adjacent = new Dictionary<Direction, Node>();
+    private bool visited = false, obstacle = false;
+
+    public bool IsObstacle => Type == NodeType.Obstacle;
+
+    public Node(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void AddAdjacent(Direction direction, Node node)
+    {
+        adjacent[direction] = node;
+        node.adjacent[OppDir[direction]] = this;
+    }
+
+    public void SetDirection(char c)
+    {
+        dir = CharDir[c];
+    }
+
+    public Node? GetNext()
+    {
+        Node? next = null;
+        var done = false;
+        while (!done)
+        {
+            if (!adjacent.ContainsKey(dir))
+            {
+                done = true;
+            }
+            else if (adjacent[dir].IsObstacle)
+            {
+                dir = RightTurnDir[dir];
+            }
+            else
+            {
+                done = true;
+                next = adjacent[dir];
+            }
+        }
+        return next;
+    }
+}
