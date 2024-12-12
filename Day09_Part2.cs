1097326b1b87b5c43776ae68fd2bf585a9461809
Blair Durkee <blairmadison11@gmail.com>
2024-12-12 13:57:39 -0500

    Update Day09_Part2.cs

--- Day09_Part2.cs
+++ Day09_Part2.cs
@@ -3,7 +3,7 @@ var drive = new Drive();
 bool free = false;
 foreach (var num in nums)
 {
-    drive.AddSector(num, free);
+    drive.AddSegment(num, free);
     free = !free;
 }
 drive.Defrag();
@@ -12,41 +12,41 @@ Console.WriteLine(drive.Checksum);
 class Drive
 {
     private int curPos = 0, nextFileId = 0;
-    private List<Sector> fileSectors = new List<Sector>();
-    private SortedList<int, Sector> freeSectors = new SortedList<int, Sector>();
+    private List<Segment> fileSegs = new List<Segment>();
+    private SortedList<int, Segment> freeSegs = new SortedList<int, Segment>();
 
-    public ulong Checksum => fileSectors.Aggregate((ulong)0, (a, c) => a + c.Checksum);
+    public ulong Checksum => fileSegs.Aggregate((ulong)0, (a, c) => a + c.Checksum);
 
-    public void AddSector(int count, bool free)
+    public void AddSegment(int count, bool free)
     {
         if (free)
         {
-            freeSectors.Add(curPos, new Sector(curPos, count, -1));
+            freeSegs.Add(curPos, new Segment(curPos, count, -1));
         }
         else
         {
-            fileSectors.Add(new Sector(curPos, count, nextFileId++));
+            fileSegs.Add(new Segment(curPos, count, nextFileId++));
         }
         curPos += count;
     }
 
     public void Defrag()
     {
-        for (int i = fileSectors.Count - 1; i >= 0; --i)
+        for (int i = fileSegs.Count - 1; i >= 0; --i)
         {
-            var fileSec = fileSectors[i];   
-            foreach (var freeSec in freeSectors.Values)
+            var fileSeg = fileSegs[i];
+            foreach (var freeSeg in freeSegs.Values)
             {
-                if (freeSec.Position > fileSec.Position) break;
-                var diff = freeSec.Size - fileSec.Size;
+                if (freeSeg.Position > fileSeg.Position) break;
+                var diff = freeSeg.Size - fileSeg.Size;
                 if (diff >= 0)
                 {
-                    fileSec.Move(freeSec.Position);
-                    freeSectors.Remove(freeSec.Position);
+                    fileSeg.Move(freeSeg.Position);
+                    freeSegs.Remove(freeSeg.Position);
                     if (diff > 0)
                     {
-                        var pos = freeSec.Position + fileSec.Size;
-                        freeSectors.Add(pos, new Sector(pos, diff, -1));
+                        var pos = freeSeg.Position + fileSeg.Size;
+                        freeSegs.Add(pos, new Segment(pos, diff, -1));
                     }
                     break;
                 }
@@ -54,7 +54,7 @@ class Drive
         }
     }
 }
-class Sector
+class Segment
 {
     private int pos, length, fileId;
 
@@ -62,7 +62,7 @@ class Sector
     public int Size => length;
     public ulong Checksum => Enumerable.Range(pos, length).Select(x => (ulong)x * (ulong)fileId).Aggregate((ulong)0, (a, c) => a + c);
 
-    public Sector(int pos, int length, int fileId)
+    public Segment(int pos, int length, int fileId)
     {
         this.pos = pos;
         this.length = length;
